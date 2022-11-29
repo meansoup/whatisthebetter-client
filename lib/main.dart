f7e301e1ca76ed147590ecf67de719e09a0828c0
meansoup <alsrnr1210@gmail.com>
2022-11-30 02:08:21 +0900

    feat: post page를 sample인 postId로 조회하도록 구현

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const Home(),
-        '/post': (context) => const Post(),
+        '/post': (context) => const GetPost(),
       },
     );
   }
