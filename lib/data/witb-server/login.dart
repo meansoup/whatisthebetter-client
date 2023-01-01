4462ae066bcebeced8c4ab47099730b4de88635f
meansoup <alsrnr1210@gmail.com>
2023-01-02 00:10:23 +0900

    feat: witb server에 대한 login 로직 구현

---
+++ lib/data/witb-server/login.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<bool> login(LoginRequest loginRequest) async {
+  var requestBody = jsonEncode(loginRequest);
+  print(requestBody);
+
+  final response = await http.post(
+    Uri.parse('https://06g3yu62c2.execute-api.ap-northeast-2.amazonaws.com/v1/login'),
+    body: jsonEncode(loginRequest),
+    headers: {
+      "content-type" : "application/json",
+      "accept" : "application/json",
+    }
+  );
+
+  if (response.statusCode == 200) {
+    var loginResponse = LoginResponse.fromJson(jsonDecode(response.body));
+    print('witbToken = ${loginResponse.witbToken}');
+    return true;
+  } else {
+    print('login error');
+    return false;
+  }
+}
+
+class LoginRequest {
+  final String idToken;
+  final String social;
+
+  const LoginRequest({
+    required this.idToken,
+    required this.social,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'idToken': idToken,
+      'social': social,
+    };
+  }
+}
+
+class LoginResponse {
+  final String witbToken;
+
+  const LoginResponse({
+    required this.witbToken,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'witbToken': witbToken,
+    };
+  }
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    return LoginResponse(
+      witbToken: json['witbToken'],
+    );
+  }
+}
