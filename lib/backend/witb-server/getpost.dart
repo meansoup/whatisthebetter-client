278cfaae4ffa78952dc41af1e849e4dfc3a89204
meansoup <alsrnr1210@gmail.com>
2023-01-04 22:52:51 +0900

    feat: witbToken이 있는 경우(null이 아닌 경우) header에 token을 넣어서 요청, 없는 경우는 넣지 않고 요청
    - token을 넣는 경우 유저가 like하는 contentId를 받아올 수 있음

--- lib/backend/witb-server/getpost.dart
+++ lib/backend/witb-server/getpost.dart
@@ -4,30 +4,28 @@ import 'dart:convert';
 import 'package:client/domain/content.dart';
 import 'package:client/domain/post.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 Future<PostData> getPost(String? witbToken, String postId) async {
-
-  Map<String, String> headers = {};
+  Response response;
   if (witbToken != null) {
-    headers = {
-      'witbToken': witbToken,
-    };
+    response = await http.get(
+        Uri.parse('https://06g3yu62c2.execute-api.ap-northeast-2.amazonaws.com/v1/post?postId=' + postId),
+        headers: {
+          'witbToken': witbToken,
+        }
+    );
+  } else {
+    response = await http.get(
+        Uri.parse('https://06g3yu62c2.execute-api.ap-northeast-2.amazonaws.com/v1/post?postId=' + postId),
+    );
   }
 
-  final response = await http.get(
-    Uri.parse('https://06g3yu62c2.execute-api.ap-northeast-2.amazonaws.com/v1/post?postId=' + postId),
-    headers: headers
-  );
-
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     print(response.body);
     var getPostResponse = GetPostResponse.fromJson(jsonDecode(response.body));
     return getPostResponse.toPostData();
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
