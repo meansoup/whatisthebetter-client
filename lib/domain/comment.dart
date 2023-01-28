a6ab46c08e5e3e3ddc6648cbc5844f6ff8d20d62
meansoup <alsrnr1210@gmail.com>
2023-01-28 19:52:13 +0900

    feat: comment domain 추가

---
+++ lib/domain/comment.dart
@@ -0,0 +1,13 @@
+class Comment {
+  final String commentId;
+  final String uid;
+  final String text;
+  final int createdAt;
+
+  const Comment({
+    required this.commentId,
+    required this.uid,
+    required this.text,
+    required this.createdAt,
+  });
+}
