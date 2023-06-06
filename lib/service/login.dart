6213f2ab4e602b63337c9d32010f68c95282a389
meansoup <alsrnr1210@gmail.com>
2023-06-06 18:40:53 +0900

    feat: 이전 로그인 코드 제거

--- lib/service/login.dart
+++ lib/service/login.dart
@@ -1,5 +1,4 @@
 import 'package:client/backend/sign/google.dart';
-import 'package:client/backend/witb-server/login.dart';
 import 'package:client/service/service_exception.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,25 +9,6 @@ class LoginWithGoogle {
   Future<void> readyToSignIn() async {
     googleSignIn.signInSilently();
   }
-}
-
-Future<String> loginIfNotLoggedIn() async {
-  final prefs = await SharedPreferences.getInstance();
-
-  var loggedInToken = prefs.getString('witbToken') ?? "";
-
-  if (loggedInToken.isNotEmpty) {
-    return loggedInToken;
-  }
-
-  var googleSignIn = await signInGoogle();
-  var authentication = await googleSignIn.currentUser?.authentication;
-  var idToken = authentication?.idToken;
-  print("google id token: $idToken");
-
-  var witbToken = await login(LoginRequest(idToken: idToken!, social: "GOOGLE"));
-  prefs.setString('witbToken', witbToken);
-  return witbToken;
 }
 
 Future<String?> getTokenIfLoggedIn() async {
