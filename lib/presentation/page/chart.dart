84145590c0ab5cdf18a0551dd57448203e938574
meansoup <alsrnr1210@gmail.com>
2023-06-25 00:12:34 +0900

    feat: widget을 적용해서 sample chart page 변경

--- lib/presentation/page/chart.dart
+++ lib/presentation/page/chart.dart
@@ -1,4 +1,5 @@
-import 'package:fl_chart/fl_chart.dart';
+import 'package:client/domain/likerate.dart';
+import 'package:client/presentation/widget/likeratechart/like_rate_pie_chart.dart';
 import 'package:flutter/material.dart';
 
 class ChartSample extends StatefulWidget {
@@ -23,30 +24,15 @@ class ChartSampleState extends State {
           Expanded(
             child: AspectRatio(
               aspectRatio: 1,
-              child: PieChart(
-                PieChartData(
-                  pieTouchData: PieTouchData(
-                    touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                      setState(() {
-                        if (!event.isInterestedForInteractions ||
-                            pieTouchResponse == null ||
-                            pieTouchResponse.touchedSection == null) {
-                          touchedIndex = -1;
-                          return;
-                        }
-                        touchedIndex = pieTouchResponse
-                            .touchedSection!.touchedSectionIndex;
-                      });
-                    },
-                  ),
-                  borderData: FlBorderData(
-                    show: false,
-                  ),
-                  sectionsSpace: 0,
-                  centerSpaceRadius: 40,
-                  sections: showingSections(),
-                ),
-              ),
+              child: LikeRatePieChartWidget(likeRates: LikeRates(
+                  [
+                    LikeRate("A", 14.7),
+                    LikeRate("B", 25.3),
+                    LikeRate("C", 8.8),
+                    LikeRate("D", 31.2),
+                    LikeRate("E", 20.0),
+                  ]
+              )),
             ),
           ),
           const SizedBox(
@@ -55,70 +41,5 @@ class ChartSampleState extends State {
         ],
       ),
     );
-  }
-
-  List<PieChartSectionData> showingSections() {
-    return List.generate(4, (i) {
-      final isTouched = i == touchedIndex;
-      final fontSize = isTouched ? 25.0 : 16.0;
-      final radius = isTouched ? 60.0 : 50.0;
-      const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            color: Colors.blue,
-            value: 40,
-            title: '40%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 1:
-          return PieChartSectionData(
-            color: Colors.yellow,
-            value: 30,
-            title: '30%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 2:
-          return PieChartSectionData(
-            color: Colors.purple,
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 3:
-          return PieChartSectionData(
-            color: Colors.green,
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        default:
-          throw Error();
-      }
-    });
   }
 }
