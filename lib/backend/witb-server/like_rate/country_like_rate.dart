1bc1e6883c6fe9842a8d9d0be21a1d58aea26ce7
meansoup <alsrnr1210@gmail.com>
2023-06-26 23:06:01 +0900

    feat: countryLikeRate api 개발

---
+++ lib/backend/witb-server/like_rate/country_like_rate.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+String baseUrl = 'https://06g3yu62c2.execute-api.ap-northeast-2.amazonaws.com';
+
+Future<CountryLikeRateResponse> countryLikeRate(String? witbToken, String postId) async {
+  Response response;
+  if (witbToken != null) {
+    response = await http.get(
+        Uri.parse(baseUrl + '/v1/post/' + postId + '/like/country/KR'),
+        headers: {
+          'witbToken': witbToken,
+        }
+    );
+  } else {
+    response = await http.get(
+      Uri.parse(baseUrl + '/v1/post/' + postId + '/like/country/KR'),
+    );
+  }
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return CountryLikeRateResponse.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+class CountryLikeRateResponse {
+  final String country;
+  final Map<String, double> likeRate;
+
+  const CountryLikeRateResponse({
+    required this.country,
+    required this.likeRate,
+  });
+
+  factory CountryLikeRateResponse.fromJson(Map<String, dynamic> json) {
+    Map<String, dynamic> likeRateJson = json['likeRate'];
+    Map<String, double> parsedLikeRate = {};
+
+    likeRateJson.forEach((key, value) {
+      parsedLikeRate[key] = value.toDouble();
+    });
+
+    return CountryLikeRateResponse(
+      country: json['country'],
+      likeRate: parsedLikeRate,
+    );
+  }
+}
